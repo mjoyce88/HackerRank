4903901f9defffe73ed0fd302f10e31e5cdda141
Mhairi Joyce <75309489+mjoyce88@users.noreply.github.com>
2022-09-21 12:56:41 +0100

    Create conditional statements (if).py

---
+++ conditional statements (if).py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+x = [3,4]
+
+if __name__ == '__main__':
+    N = int(input().strip())
+
+if N in range(6, 20+1, 2) or N%2 != 0: 
+    print("Weird")
+else: print("Not Weird")
