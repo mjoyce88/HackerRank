dfaa4cdff8eba4fc7d280708be34978c6225999b
Mhairi Joyce <75309489+mjoyce88@users.noreply.github.com>
2022-10-03 14:52:37 +0100

    Update Binary.py
    binary checker

--- Binary.py
+++ Binary.py
@@ -1,1 +1,20 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    b = str(bin(n))[2:]
+    p = re.compile(r'1+')
+    matches = p.finditer(b)
+    result = []
+    for m in matches:
+        result.append(m.end() - m.start())
+        
+    print(max(result))
